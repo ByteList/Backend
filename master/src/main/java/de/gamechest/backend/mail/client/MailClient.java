f11cbe8005959acf66d5f033935ab14956266b40
ByteList <niklas.bytelist@gmail.com>
2018-04-24 18:40:32 +0200

    Test

--- master/src/main/java/de/gamechest/backend/mail/client/MailClient.java
+++ master/src/main/java/de/gamechest/backend/mail/client/MailClient.java
@@ -40,38 +40,14 @@ public class MailClient {
         try {
             String content = Resources.toString(Resources.getResource("mails/register.html"), Charset.forName("UTF-8"))
                     .replace("#{user.name}", user).replace("#{user.verifyUrl}", "https://game-chest.de/register/"+verifyCode+"/");
-//            return sendHtmlMail("Registrierung auf game-chest.de", content, mail);
-            sendMail(mail, user, "Registrierung auf game-chest.de", content);
-            return true;
+            return sendMail(mail, user, "Registrierung auf game-chest.de", content);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-//    private boolean sendHtmlMail(String subject, Object content, String toAddress) {
-//        try {
-//            MimeMessage message = new MimeMessage(session);
-//
-//            message.setFrom(new InternetAddress(fromAddress, "GameChest"));
-//            message.setSender(new InternetAddress("bytelist.de"));
-//            message.setSentDate(new Date());
-//
-//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(toAddress));
-//
-//            message.setSubject(subject);
-//            message.setContent(content, "text/html");
-//
-//            Transport.send(message);
-//            System.out.println("[Mail] Sent mail to "+toAddress+": "+subject);
-//            return true;
-//        } catch (MessagingException | UnsupportedEncodingException ex) {
-//            ex.printStackTrace();
-//        }
-//        return false;
-//    }
-
-    private void sendMail(String mail, String user, String subject, String html) {
+    private boolean sendMail(String mail, String user, String subject, String html) {
         try {
             Email email = EmailBuilder.startingBlank()
                     .to(user, mail)
@@ -82,9 +58,10 @@ public class MailClient {
                     .signWithDomainKey(privateKeyData, "bytelist.de", "apr2018")
                     .buildEmail();
             this.mailer.sendMail(email);
+            return true;
         } catch (AddressException e) {
             e.printStackTrace();
         }
-
+        return false;
     }
 }
