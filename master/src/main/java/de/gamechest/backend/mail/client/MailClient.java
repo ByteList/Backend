698fd111b655e019afa99a629e239cd05e0f6344
ByteList <niklas.bytelist@gmail.com>
2018-04-25 16:28:43 +0200

    Test

--- master/src/main/java/de/gamechest/backend/mail/client/MailClient.java
+++ master/src/main/java/de/gamechest/backend/mail/client/MailClient.java
@@ -1,7 +1,6 @@
 package de.gamechest.backend.mail.client;
 
 import com.google.common.io.Resources;
-import de.gamechest.backend.Backend;
 import org.simplejavamail.email.Email;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.Mailer;
@@ -11,13 +10,8 @@ import org.simplejavamail.mailer.config.TransportStrategy;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 
 /**
  * Created by ByteList on 08.03.2018.
@@ -26,53 +20,54 @@ import java.security.spec.PKCS8EncodedKeySpec;
  */
 public class MailClient {
 
-    private byte[] privateKeyData;
+    private File privateKeyData;
     private final Mailer mailer;
     private final String fromAddress;
 
     public MailClient(File privateKeyFile, String host, String user, String password) {
+        this.privateKeyData = privateKeyFile;
         this.mailer = MailerBuilder
                 .withSMTPServer(host, 587, user, password)
                 .withTransportStrategy(TransportStrategy.SMTP)
                 .buildMailer();
         this.fromAddress = user;
 
-        Backend.getInstance().runAsync(()-> {
-            StringBuilder keyAsStr = new StringBuilder();
-            FileInputStream fileInputStream = null;
-            try {
-                fileInputStream = new FileInputStream(privateKeyFile);
-
-                int content;
-                while ((content = fileInputStream.read()) != -1) {
-                    keyAsStr.append((char) content);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (fileInputStream != null) {
-                        fileInputStream.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            byte[] key = keyAsStr.toString().getBytes();
-
-            try {
-                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(key);
-                try {
-                    this.privateKeyData = keyFactory.generatePrivate(keySpec).getEncoded();
-                } catch (InvalidKeySpecException e) {
-                    e.printStackTrace();
-                }
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-        });
+//        Backend.getInstance().runAsync(()-> {
+//            StringBuilder keyAsStr = new StringBuilder();
+//            FileInputStream fileInputStream = null;
+//            try {
+//                fileInputStream = new FileInputStream(privateKeyFile);
+//
+//                int content;
+//                while ((content = fileInputStream.read()) != -1) {
+//                    keyAsStr.append((char) content);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    if (fileInputStream != null) {
+//                        fileInputStream.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            byte[] key = keyAsStr.toString().getBytes();
+//
+//            try {
+//                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+//                PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(key);
+//                try {
+//                    this.privateKeyData = keyFactory.generatePrivate(keySpec).getEncoded();
+//                } catch (InvalidKeySpecException e) {
+//                    e.printStackTrace();
+//                }
+//            } catch (NoSuchAlgorithmException e) {
+//                e.printStackTrace();
+//            }
+//        });
     }
 
     public boolean sendRegisterMail(String mail, String user, String verifyCode) {
